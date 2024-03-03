a505c4f463e8caa50f25f7c4c0d00228980b0d18
Kelly Cruz <30739866+kellypc@users.noreply.github.com>
2024-03-03 16:52:59 -0300

    Create bank billet

--- app/controllers/bank_billets_controller.rb
+++ app/controllers/bank_billets_controller.rb
@@ -8,11 +8,34 @@ class BankBilletsController < ApplicationController
   def show
   end
 
+  def new
+    @bank_billet = BankBillet.new
+  end
+
+  def create
+    @bank_billet = BankBillet.new(bank_billet_params)
+
+    if @bank_billet.save
+      flash[:success] = "Object successfully created"
+      redirect_to @bank_billet, status: :created
+    else
+      flash[:error] = "Something went wrong"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_bank_billet
     @bank_billet = BankBillet.find(params[:id])
   end
-  
+
+  private
+
+  def bank_billet_params
+    params.require(:bank_billet).permit(:amount, :expire_at, :customer_person_name, :customer_cnpj_cpf, :customer_state,
+                                         :customer_city_name, :customer_zipcode, 
+                                         :customer_address, :customer_address_neighborhood)
+  end
 end
     
--- spec/requests/bank_billets_spec.rb
+++ spec/requests/bank_billets_spec.rb
@@ -12,10 +12,28 @@ RSpec.describe BankBilletsController, type: :controller do
 
   context 'Get #Show' do
     let(:bank_billet) { FactoryBot.create(:bank_billet) }
-    
+
     it 'returns status code 200 and render to edit page' do
       get :show, params: { id: bank_billet.id}
     end
-    
+  end
+
+  context 'Post #create' do
+    it 'returns created status' do
+      params = {
+        amount: 2.0,
+        expire_at: "2024-03-02",
+        customer_person_name: "Myperson name",
+        customer_cnpj_cpf: "My person cpf",
+        customer_state: "Myperson state",
+        customer_city_name: "MyString",
+        customer_zipcode: "MyString",
+        customer_address: "MyString",
+        customer_address_neighborhood: "MyString"
+      }
+
+      expect { post(:create, params: { bank_billet: params })}.to change(BankBillet, :count).by(1)
+      expect(response).to have_http_status(:created)
+    end
   end
 end
