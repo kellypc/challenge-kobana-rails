64ce0f8a8aee7eed20421982909273bc251f74ba
Kelly Cruz <30739866+kellypc@users.noreply.github.com>
2024-03-03 10:36:38 -0300

    Show bank billet

--- app/controllers/bank_billets_controller.rb
+++ app/controllers/bank_billets_controller.rb
@@ -1,6 +1,18 @@
 class BankBilletsController < ApplicationController
+  before_action :set_bank_billet, only: [:show]
+  
   def index
     @bank_billets = BankBillet.all
   end
+
+  def show
+  end
+
+  private
+
+  def set_bank_billet
+    @bank_billet = BankBillet.find(params[:id])
+  end
+  
 end
     
--- spec/requests/bank_billets_spec.rb
+++ spec/requests/bank_billets_spec.rb
@@ -2,11 +2,20 @@ require 'rails_helper'
 
 RSpec.describe BankBilletsController, type: :controller do
 
-  context "Get #index" do
-    it "does have index page" do
+  context 'Get #index' do
+    it 'returns status code 200' do
       get :index
 
       expect(response).to have_http_status(200)
     end
+  end
+
+  context 'Get #Show' do
+    let(:bank_billet) { FactoryBot.create(:bank_billet) }
+    
+    it 'returns status code 200 and render to edit page' do
+      get :show, params: { id: bank_billet.id}
+    end
+    
   end
 end
