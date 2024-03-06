94a9e1e0758eff9378a59a3127d4db7e782b548c
Kelly Cruz <30739866+kellypc@users.noreply.github.com>
2024-03-06 11:44:55 -0300

    Add validations

--- app/models/bank_billet.rb
+++ app/models/bank_billet.rb
@@ -1,2 +1,5 @@
 class BankBillet < ApplicationRecord
+  validates :amount, :expire_at, :customer_person_name, :customer_cnpj_cpf, presence: true
+  validates :customer_state, :customer_city_name, :customer_zipcode, presence: true
+  validates :customer_address, :customer_address_neighborhood, presence: true
 end
